b9965be222041229d8a1fd73efb8b5761c9735db
kasidej02 <kasidej.kammool@gmail.com>
2020-01-10 15:55:21 +0700

    number 2

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    float sum=0,x,y;
+     while(x!=0){
+        
+    cout<<"Enter x: ";
+    cin>>x;
+
+    if(x==0){
+        cout<<"sum = "<<sum;
+    }else
+    {
+        sum=sum+x;
+    }
+    if(x<0){
+        sum=sum-x;
+    }
+    }
+    return 0;
+}
