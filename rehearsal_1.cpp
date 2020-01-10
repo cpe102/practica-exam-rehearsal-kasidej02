6ee5ea52cee905edc876da080815e90327c9e498
kasidej02 <kasidej.kammool@gmail.com>
2020-01-10 14:51:40 +0700

    number 1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    double GPA;
+    cout<<"What is your name? : ";
+    cin>>name;
+    cout<<"What is your GPA? : ";
+    cin>>GPA;
+    if(GPA>=3.50){
+        cout<<name<<" InW "<<"Jrim "<<"Jrim!!!";
+    }else
+    {
+        cout<<"Try harder, "<<name<<"!!!";
+    }
+    return 0;
+    
+}
 
